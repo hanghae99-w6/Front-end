ea3b46ace58f3e491a4418723779fb404613e851
lucy-pill <schmidtiana95@gmail.com>
2022-08-18 17:11:51 +0900

    add image

--- src/static/image/image.js
+++ src/static/image/image.js
@@ -1,4 +1,21 @@
-import logoHeader from "./logoHeader.png";
-import logoFooter from "./logoFooter.png";
-import nope from "./landing/nope.jfif"
-export { logoHeader, logoFooter, nope };
+import logoHeader from './logoHeader.png';
+import logoFooter from './logoFooter.png';
+import drama_1 from './landing/drama_1';
+import drama_2 from './landing/drama_2';
+import entertain_8 from './landing/entertain_8';
+import entertain_9 from './landing/drentertain_9ama_1';
+import entertain_10 from './landing/entertain_10';
+import movie_20 from './landing/movie_20';
+import movie_21 from './landing/movie_21';
+
+export {
+  logoHeader,
+  logoFooter,
+  drama_1,
+  drama_2,
+  entertain_8,
+  entertain_9,
+  entertain_10,
+  movie_20,
+  movie_21,
+};
